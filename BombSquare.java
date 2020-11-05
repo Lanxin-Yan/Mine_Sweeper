2241e1fdce74f8c463341bc8eb014fd92710eb60
lanxinyan <Lanxin-Yan@users.noreply.github.com>
2020-11-05 20:19:46 +0000

    Bug Fixes

--- BombSquare.java
+++ BombSquare.java
@@ -1,19 +1,14 @@
-import javax.swing.*;
-
 
 public class BombSquare extends GameSquare
 {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 8456708344160245595L;
-    private GameBoard board; // Object reference to the GameBoard this square is part of.
-    private boolean hasBomb;                            // True if this squre contains a bomb. False otherwise.
 
-    public static final int MINE_PROBABILITY = 10;
-    
-    //private int num;
-    Icon bomb = new ImageIcon("images/bomb.png");
+    private GameBoard board;                            // Object reference to the GameBoard this square is part of.
+    private boolean hasBomb;                            // True if this squre contains a bomb. False otherwise.
+    private int count;
+    BombSquare compBomb;
+    private boolean blank;
+
+    public static final int MINE_PROBABILITY = 5;
 
 	public BombSquare(int x, int y, GameBoard board)
 	{
@@ -21,30 +16,16 @@ public class BombSquare extends GameSquare
 
         this.board = board;
         this.hasBomb = ((int) (Math.random() * MINE_PROBABILITY)) == 0;
+        this.blank = true;
     }
 
-
-    public void leftClicked() {
-        //System.out.println(hasBomb);
+    public void leftClicked()
+    {
         if (hasBomb){
             this.setImage("images/bomb.png");
         }
         else{
-            int index = buttonNum(this.getXLocation(), this.getYLocation());
-            //System.out.println(this.getXLocation());
-            //System.out.println(this.getYLocation());
-            //buttonNum(this.getXLocation(), this.getYLocation());
-            System.out.println("The num on this square should be:" + buttonNum(this.getXLocation(), this.getYLocation()));
-            if (index == 0) this.setImage("images/0.png");
-            if (index == 1) this.setImage("images/1.png");
-            if (index == 2) this.setImage("images/2.png");
-            if (index == 3) this.setImage("images/3.png");
-            if (index == 4) this.setImage("images/4.png");
-            if (index == 5) this.setImage("images/5.png");
-            if (index == 6) this.setImage("images/6.png");
-            if (index == 7) this.setImage("images/7.png");
-            if (index == 8) this.setImage("images/8.png");
-            if (index == 9) this.setImage("images/9.png");
+            recursiveCall(this.getXLocation(), this.getYLocation());
             }
         }
     
@@ -53,27 +34,30 @@ public class BombSquare extends GameSquare
         setImage("images/flag.png");
     }
 
-    public int buttonNum(int x, int y){
-        int count = 0;
+    public void recursiveCall(int x, int y) {
 
-        for (int a = - 1; a < 2; a++){
-            for (int b = -1 ; b < 2; b++){
-
-                BombSquare compBomb = (BombSquare) board.getSquareAt(a + x, b + y);
-
-                System.out.println("x is" + (a + x));
-                System.out.println("y is" + (b + y));
-                try{ 
-                    if(compBomb.hasBomb){
-                        System.out.println("Got a bomb");
-                        count ++;
-                    }
-                    else{
-                        System.out.println("No bomb");
-                    }
-                }catch(NullPointerException e){ System.out.println("Null error");} 
+        if (x < 0 || y < 0 || x > 29 || y > 29) return;
+    
+        for (int dx = -1; dx < 2; ++dx)
+            for (int dy = -1; dy < 2; ++dy) {
+                try {
+                    BombSquare nei = (BombSquare)board.getSquareAt(x+dx,y+dy);
+                    if (nei.hasBomb) count++;
+                
+                } catch (NullPointerException e) {
+                }   
             }
-        }
-         return count;
-    }
+
+        this.setImage("images/" + count + ".png");
+        this.blank = false;
+    
+        if (count > 0) return;
+        for (int dx = -1; dx < 2; ++dx)
+            for (int dy = -1; dy < 2; ++dy) {
+                if (x+dx < 0 || x+dx > 29 || y+dy < 0 || y+dy > 29) continue;
+                BombSquare nei = (BombSquare)board.getSquareAt(x+dx,y+dy);
+                if (nei.blank)
+                    nei.recursiveCall(x+dx,y+dy);
+                }
+            }
 }
